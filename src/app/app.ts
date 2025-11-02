90daf707d2a2615de5fd7301b526c5e259308a71
Balaji-7 <sweetbalaji2000@gmail.com>
2025-11-02 16:59:27 +0530

    Added responsive toolbar and mobile hamburger visibility fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,18 @@ import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, RouterModule,MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Customer Agreement Management System');
+   menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
